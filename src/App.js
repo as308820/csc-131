9ad579a91aefc52d819cd51121de07a018464f1e
Osvaldo <os308800@gmail.com>
2025-03-18 13:44:03 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
+//functions
+import {getTest} from "./functions/test";
 
 function App() {
+  const [data, setData] = useState("HELLO WORLD");
+
+  useEffect(() => {
+    getTest()
+      .then((res) => {
+        setData(res.message);
+      })
+      .catch((err) => console.log(err));    
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{data}</h1>
     </div>
   );
 }
