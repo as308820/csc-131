236565cd19dc633f146cabcb678e647a6ab95b81
Aaron Rodriguez <aaronrodriguez@Aarons-MacBook-Air-9.local>
2025-03-26 17:31:54 -0700

    Added Contact, Home, About page + added React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import { useState, useEffect } from 'react';
 
 
@@ -17,9 +21,28 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-     <h1>{data}</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>{data}</h1>
+        <p>This is a simple React frontend</p>
+
+        {/* Navigation Links */}
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+
+        {/*Define Routes */}
+        <Routes>
+            <Route path= "/" element={<Home />} />
+            <Route path= "/about" element={<About />} />
+            <Route path= "/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
