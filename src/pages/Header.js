8afbb2fbbdd8bcdb02be187981aac283a743596c
Aaron Rodriguez <aaronrodriguez@Aarons-MacBook-Air-9.local>
2025-04-08 21:59:31 -0700

    Created Header.js file

---
+++ src/pages/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; //For navigation
+
+const Header = ()=> {
+    return (
+        <header>
+            <h1>Quiz App</h1>
+            <nav className="nav-links">
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+            </nav>
+            {/*TODO: Add styling to the header and navigation links later */}
+        </header>
+    );
+};
+
+export default Header;
+
