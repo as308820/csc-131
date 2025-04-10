4920c955bf58a022db61aba32e388ad99be84c69
Isaac Benitez Cisneros <isaacbenitezcisneros@Isaacs-MacBook-Air-154.local>
2025-04-10 11:59:27 -0700

    Had to moditify the app.jsx to rename the contacts imports from contact --> contacts

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Contacts from "./pages/Contacts";
 import Credits from "./pages/Credits";
 import { useState, useEffect } from 'react';
 
@@ -33,7 +33,7 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/contacts">Contacts</Link></li>
             <li><Link to="/credits">Credits</Link></li>
           </ul>
         </nav>
@@ -42,7 +42,7 @@ function App() {
         <Routes>
             <Route path= "/" element={<Home />} />
             <Route path= "/about" element={<About />} />
-            <Route path= "/contact" element={<Contact />} />
+            <Route path= "/contacts" element={<Contacts />} />
             <Route path= "/credits" element={<Credits />} />
         </Routes>
       </div>
