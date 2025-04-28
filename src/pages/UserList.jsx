09c7328ed58a14ae598eb67162ec5d9acc876f5b
Joshua James <blard1337@gmail.com>
2025-04-28 06:40:28 -0700

    userlist styling polish

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from '../axios';
 import { useNavigate } from 'react-router-dom';
 import './UserList.css';
+import { useAccessibility } from '../accessibility/AccessibilityContext';
 
 const UserList = () => {
     const [admins, setAdmins] = useState([]);
@@ -10,6 +11,7 @@ const UserList = () => {
     const navigate = useNavigate();
     const [showAdmins, setShowAdmins] = useState(false);
     const [showUsers, setShowUsers] = useState(false);
+    const { textSize } = useAccessibility();
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -34,32 +36,42 @@ const UserList = () => {
         <h2>User List</h2>
 
         <h3>
-            <button className="user-toggle-button" onClick={() => setShowAdmins(!showAdmins)}>
-                {showAdmins ? '↓' : '→'} Admin
+            <button
+                className="user-toggle-button"
+                style={{ fontSize: `${textSize}px` }}
+                onClick={() => setShowAdmins(!showAdmins)}
+            >
+                {showAdmins ? '↓' : '→'} Admins
             </button>
-            </h3>
-            {showAdmins && (
-            <div>
-                {admins.map((admin) => (
+        </h3>
+        <div className={`user-list-content ${showAdmins ? 'show' : ''}`}>
+            {admins.map((admin) => (
                 <p key={admin._id} className="user-entry">{admin.name}</p>
-                ))}
-            </div>
-        )}
+            ))}
+        </div>
+    
 
         <h3>
-            <button className="user-toggle-button" onClick={() => setShowUsers(!showUsers)}>
+            <button
+                className="user-toggle-button"
+                style={{ fontSize: `${textSize}px` }}
+                onClick={() => setShowUsers(!showUsers)}
+            >
                 {showUsers ? '↓' : '→'} Users
             </button>
         </h3>
-        {showUsers && (
-            <div>
-                {users.map((user) => (
-                    <p key={user._id} className="user-entry">{user.name}</p>
-                ))}
-            </div>
-        )}
+        <div className={`user-list-content ${showUsers ? 'show' : ''}`}>    
+            {users.map((user) => (
+                <p key={user._id} className="user-entry">{user.name}</p>
+            ))}
+        </div>
 
-        <button className="return-home-button" onClick={() => navigate('/')}>
+
+        <button
+            className="return-home-button"
+            style={{ fontSize: `${textSize}px` }}
+            onClick={() => navigate('/')}
+        >
             Return to Home
         </button>
         </div>
