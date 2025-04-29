fc7d9a34df2edd4050f8bca37410b2951da2480f
Aaron <rodriguez0420022gmail.com>
2025-04-28 20:16:33 -0700

    Home changes v2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import './Home.css';
 
+
 function Home() {
   return (
-    <div className="home-hero">
-      <h1 className="home-title">AlphaAnswer</h1>
-      <p className="home-subtitle">Take and Create Quizzes</p>
+    <div className="home-container">
+      <div className="text-section">
+        <h1 className="big-name">AlphaAnswer</h1>
+        <p className="big-text">Take and Create Quizzes</p>
+      </div>
+      <div className="home-image-box">
+          <img src={"Headshots/Quiz.jpg"} alt="Quiz" />
+      </div>
     </div>
   );
 }
